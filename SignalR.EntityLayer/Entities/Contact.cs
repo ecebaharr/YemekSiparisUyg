9d18f5dbe4fa51c4540897fa4ad5f5d2c1c67226
ecebaharr <ecebahaarr@gmail.com>
2025-03-17 05:11:31 +0300

    entityler oluşturuldu

---
+++ SignalR.EntityLayer/Entities/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+
+        public string Phone { get; set; }        
+        public string Mail { get; set; }
+
+        public string FooterDescripton { get; set; }
+
+    }
+}
